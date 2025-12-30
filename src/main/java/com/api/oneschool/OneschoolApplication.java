ea6366f4e1757ccab0423051c18e7b6f68af5559
Ezenna Charles <charlesezenna@gmail.com>
2025-12-30 12:14:39 +0000

    project set up

---
+++ src/main/java/com/api/oneschool/OneschoolApplication.java
@@ -0,0 +1,13 @@
+package com.api.oneschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneschoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/oneschool/OneschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.oneschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
